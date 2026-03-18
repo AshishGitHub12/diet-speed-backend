ddbcebedeafc1715190f7f6ef6418f1c0b97c08a
Ashish Saini <ashishsaini2712@gmail.com>
2026-03-18 23:30:27 +0530

    Clean repo: remove ignored files from tracking & Create Home Api

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     OnboardingStep1View,
     OnboardingStep2View,
-    OnboardingStep3View
+    OnboardingStep3View,
+    HomeView
 )
 
 urlpatterns = [
     path("onboarding/step1/", OnboardingStep1View.as_view()),
     path("onboarding/step2/", OnboardingStep2View.as_view()),
     path("onboarding/step3/", OnboardingStep3View.as_view()),
+    path("home/", HomeView.as_view(), name="home"),
 ]
